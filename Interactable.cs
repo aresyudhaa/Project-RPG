0486d93bf05b1b5aace1ebb035165641861a5e2c
aresyudhaa <62238877+aresyudhaa@users.noreply.github.com>
2020-05-11 03:55:01 +0700

    Add files via upload

---
+++ Interactable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+    public Transform interactionTransform;
+
+
+    bool isFocus = false;
+    Transform player;
+
+    bool hasInteracterd = false;
+
+    public virtual void Interact ()
+    {
+        //Will be Overwritten
+        UnityEngine.Debug.Log ("Interacting with "+transform.name);
+
+    }
+
+    void Update ()
+    {
+        if (isFocus && !hasInteracterd)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= radius)
+            {
+                Interact();
+                hasInteracterd = true;
+            }
+        }
+    }
+
+    public void OnFocused (Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracterd = false;
+    }
+
+    public void OnDefocused ()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracterd = false;
+    }
+
+    void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+}
